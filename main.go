a9bd0541a93ef91c9c2dd6f73b183f31aa159321
Guillermo Santos <guillermo.santosba@gmail.com>
2022-07-13 14:45:57 -0600

    Cleaned main.go file by removing commented code.

--- main.go
+++ main.go
@@ -58,55 +58,6 @@ func main() {
 		}
 	}
 
-
-	// EJEMPLOS EN CLASE
-
-	// Dibujar estatica
-	// for x := 0; x < 960; x++ {
-	// 	for y := 0; y < 540; y++ {
-	// 		renderer.GlPoint(gl.Point{rand.Float32(),rand.Float32()})
-	// 	}
-	// }
-
-	// Colores Random
-	// for x := 0; x < 960; x++ {
-	// 	for y := 0; y < 540; y++ {
-	// 		renderer.GlColor(rand.Float32(),rand.Float32(),rand.Float32())
-	// 		renderer.GlPoint(gl.Point{float32(x),float32(y)})
-	// 	}
-	// }
-
-	// Dibujando linea
-	// for x := 0; x < 960; x++ {
-	// 	renderer.GlPoint(gl.Point{float32(x),0})		
-	// }
-
-	// Ejemplo Galaxias
-	// for x := 0; x < 960; x++ {
-	// 	for y := 0; y < 540; y++ {
-	// 		if rand.Float32() > 0.98 {
-	// 			brightness := rand.Float32() / 2 + 0.5
-	// 			size := rand.Intn(3)
-	// 			if size == 0 {
-	// 				renderer.GlColor(1, 0, 0)
-	// 				renderer.GlPoint(gl.Point{float32(x),float32(y)})
-	// 			} else if size == 1 {
-	// 				renderer.GlColor(1, 1, 0)
-	// 				renderer.GlPoint(gl.Point{float32(x + 1),float32(y)})
-	// 				renderer.GlPoint(gl.Point{float32(x + 1),float32(y + 1)})
-	// 				renderer.GlPoint(gl.Point{float32(x),float32(y + 1)})
-	// 			} else if size == 2 {
-	// 				renderer.GlColor(brightness, brightness, brightness)
-	// 				renderer.GlPoint(gl.Point{float32(x),float32(y + 1)})
-	// 				renderer.GlPoint(gl.Point{float32(x),float32(y)})
-	// 				renderer.GlPoint(gl.Point{float32(x + 1),float32(y)})
-	// 				renderer.GlPoint(gl.Point{float32(x - 1),float32(y)})
-	// 				renderer.GlPoint(gl.Point{float32(x),float32(y - 1)})
-					
-	// 			}
-	// 		}
-	// 	}
-	// }
 	
 	renderer.GlFinish("out.bmp")
 	fmt.Println("----------      FINISHED      ----------")	
