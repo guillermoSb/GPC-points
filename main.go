035ee6b1bfd932e1ca7282be9a33ba928c5ed8f8
Guillermo Santos <guillermo.santosba@gmail.com>
2022-07-19 17:41:32 -0600

    Cleared the code

--- main.go
+++ main.go
@@ -14,37 +14,6 @@ func main() {
 	renderer.GlClearColor(0.1,0.1,0.1)
 	renderer.GlColor(1,0,0)
 	renderer.GlClear()
-
-	// Square:
-	renderer.GLLine(gl.Point{50, 50}, gl.Point{50, 200}) // |
-	renderer.GLLine(gl.Point{50, 200}, gl.Point{200, 200}) // -
-	renderer.GLLine(gl.Point{200, 200}, gl.Point{200, 50}) // |
-	renderer.GLLine(gl.Point{200, 50}, gl.Point{50, 50}) // -
-
-	// Triangle
-
-	renderer.GLLine(gl.Point{300, 50}, gl.Point{500, 50}) // -
-	renderer.GLLine(gl.Point{300, 50}, gl.Point{400, 200}) // -
-	renderer.GLLine(gl.Point{500, 50}, gl.Point{400, 200}) // -
-
-	// Triangle 90 degrees
-	renderer.GLLine(gl.Point{600,50}, gl.Point{750,50}) // |
-	renderer.GLLine(gl.Point{600,50},gl.Point{600,200}) // _
-	renderer.GLLine(gl.Point{600,200}, gl.Point{750,50}) // \
-
-	// Hexagon
-	// angle := math.Pi - (2/3) * math.Pi
-	// yprev := 50.0
-	// xprev := 600.0
-	// for i := 0; i < 6; i++ {
-	// 	renderer.GLLine((gl.Point{float32(xprev), float32(yprev)}), gl.Point{float32(math.Cos(200 * angle)), float32(math.Sin(200 * angle))})
-	// }
-
-
-
-	
-	
-
 	
 	renderer.GlFinish("out.bmp")
 	fmt.Println("----------      FINISHED      ----------")	
